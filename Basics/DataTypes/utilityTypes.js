0c89bece1f333796b182bc965e9d9e10ceaa9a43
Arvind <jsrarvind12@gmail.com>
2026-05-20 19:47:36 +0530

    done with left utility types

--- Basics/DataTypes/utilityTypes.js
+++ Basics/DataTypes/utilityTypes.js
@@ -1,8 +1,10 @@
 "use strict";
+// Partial
 var studentDetails = {
     name: "Arvind",
     age: 21
 };
+// Required
 var studentDetails2 = {
     name: "Arvind",
     age: 21,
@@ -10,6 +12,7 @@ var studentDetails2 = {
     college: "IIT DELHI",
     semester: 4
 };
+//Readonly
 var studentDetails3 = {
     name: "Arvind",
     age: 21,
@@ -17,7 +20,25 @@ var studentDetails3 = {
     semester: 4,
     college: "IIT DELHI",
 };
+//Pick
 var studentDetails4 = {
     name: "Arvind",
     college: "IIT ROORKE"
+};
+// Omit
+var studentDetails5 = {
+    name: "Arvind",
+    age: 21,
+    location: "Delhi",
+    college: "IIT DELHI",
+};
+var apicalls;
+apicalls = "loading"; // now  here you can not assign error to apicall
+var datatypeswithnonnullableDetail;
+datatypeswithnonnullableDetail = "arvind"; // now here in this var you cann't assing null or undefined vale
+var siteroleoption = 'admin';
+var rolename = {
+    admin: "arvind",
+    user: "harsh",
+    guest: "peter"
 };
--- Basics/DataTypes/utilityTypes.ts
+++ Basics/DataTypes/utilityTypes.ts
@@ -5,12 +5,13 @@ interface collegeType{
     college:string,
     semester:number
 }
-
+// Partial
 var studentDetails:Partial<collegeType>={
     name:"Arvind",
     age:21
 }
 
+// Required
 var studentDetails2:Required<collegeType>={
     name:"Arvind",
     age:21,
@@ -19,6 +20,7 @@ var studentDetails2:Required<collegeType>={
     semester:4
 }
 
+//Readonly
 var studentDetails3:Readonly<collegeType>={
     name:"Arvind",
     age:21,
@@ -27,7 +29,41 @@ var studentDetails3:Readonly<collegeType>={
     college:"IIT DELHI",
 }
 
+//Pick
 var studentDetails4:Pick<collegeType,'name'|'college'> ={
     name:"Arvind",
     college:"IIT ROORKE"
+}
+
+// Omit
+var studentDetails5:Omit<collegeType,'semester'>={
+    name:"Arvind",
+    age:21,
+    location:"Delhi",
+    college:"IIT DELHI",
+}
+
+//Exclude
+
+type Apistatus = "pending" | "resolve" | "loading" | "error"
+
+var apicalls:Exclude<Apistatus,'error'>;
+apicalls="loading" // now  here you can not assign error to apicall
+
+//nonnullable
+
+type datatypeswithnonnullable = string | number | boolean | null | undefined;
+
+var datatypeswithnonnullableDetail:NonNullable<datatypeswithnonnullable>;
+datatypeswithnonnullableDetail="arvind" // now here in this var you cann't assing null or undefined vale
+
+
+//Record
+
+type siterole = 'admin' | 'user' | 'guest'
+var siteroleoption:siterole='admin';
+var rolename:Record<siterole,string>={
+    admin:"arvind",
+    user:"harsh",
+    guest:"peter"
 }
